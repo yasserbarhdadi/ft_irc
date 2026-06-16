1888a5cc055b50614f5ff23f6b125a56137c88d1
didyounes <youneseljoulali05@gmail.com>
2026-06-16 17:56:46 +0100

    Started working on the actual server

--- didyounes/Server.cpp
+++ didyounes/Server.cpp
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Server.hpp"
 
+Server::Server(int f_port, std::string f_password) : _port(f_port) , _password(f_password) , _server_fd(-1)
+{
+
+}
+Server::~Server()
+{
+    if (_server_fd != -1)
+        close(_server_fd);
+
+}
+void Server::init()
+{
+    int opt = 1;
+    _server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (_server_fd == -1)
+        throw std::runtime_error("Failed to create socket");
+    if ((setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) == -1)
+        throw std::runtime_error("Address already in use");
+    if ((fcntl(_server_fd, F_SETFL, O_NONBLOCK)) == -1 )
+        throw std::runtime_error("something blocked");
+}
--- didyounes/Server.hpp
+++ didyounes/Server.hpp
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERVER_HPP 
+#define SERVER_HPP
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+
+
+class Server
+{
+    private:
+            int _server_fd;
+            int _port;
+            std::string _password;
+    public:
+            Server(int f_port, std::string f_password);
+            void    init();
+            ~Server();
+};
+#endif
